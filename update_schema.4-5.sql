a99b5fbd5d0b136c6868c71bb9300e29fe1302de
Nick Prater <nick@npbroadcast.com>
2017-06-29 14:48:19 +0100

    Add profile_id foreign key constraints and playlists table

--- update_schema.4-5.sql
+++ update_schema.4-5.sql
@@ -1,5 +1,19 @@
-
 UPDATE config SET parameter_value = 5 WHERE parameter_key = 'schema_version';
+
 ALTER TABLE clips ADD COLUMN deleted_timestamp TIMESTAMP NULL;
+
+ALTER TABLE clips MODIFY profile_id INT UNSIGNED NOT NULL;
+ALTER TABLE buttons MODIFY profile_id INT UNSIGNED NOT NULL;
+ALTER TABLE channels MODIFY profile_id INT UNSIGNED NOT NULL;
+
+CREATE TABLE playlists(
+  playlist_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(200) NOT NULL DEFAULT '',
+  profile_id INT UNSIGNED NOT NULL,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  data JSON NOT NULL,
+  FOREIGN KEY (profile_id) REFERENCES profiles (profile_id)
+);
 
 
